17fda30adeded0134435802fd946af98277ce6fc
shunjinchan <shunjinchan@gmail.com>
2018-10-07 23:32:23 +0800

    删除 link unlink 方法，增加 mapState 方法

--- utils/wxStore.js
+++ utils/wxStore.js
@@ -2,7 +2,7 @@
  * @Author: shunjinchan
  * @Date: 2018-10-03 07:18:38
  * @Last Modified by: shunjinchan
- * @Last Modified time: 2018-10-04 15:35:03
+ * @Last Modified time: 2018-10-07 23:31:33
  * 全局状态管理
  * 目的：同步一下全局变量所绑定的视图
  * 为什么要加找个模块：
@@ -13,9 +13,17 @@
  * 用法：在页面的 onload 事件中使用 link 方法进行数据关联，这样页面就可以拿到全局数据，并且当使用 setState 改变全局数据时，做了绑定操作的页面的数据与视图也会相应发生变化
  */
 
-let linkId = 1
-let state = {}
-const subs = [] // 页面对象数组
+let linkId = 0
+
+/**
+ * 将对象字符串路径转换成数组形式
+ * @param {string} path
+ */
+const getPath = path => {
+  let arr = path.replace(/\[|(].)|\]/g, '.').split('.')
+  if (arr[arr.length - 1] === '') arr.pop() // 处理空白字符
+  return arr
+}
 
 /**
  * 更新 state
@@ -24,20 +32,13 @@ const subs = [] // 页面对象数组
  * @param {any} value val
  */
 const updateByPath = (object, path, value) => {
-  let arr = path.replace(/\[|(].)|\]/g, '.').split('.')
   let current = object
 
-  if (arr[arr.length - 1] === '') arr.pop() // 处理空白字符
-  // 要求在创建实例之前，就声明所有的根级属性
-  if (!object.hasOwnProperty(arr[0])) {
-    throw new Error('setState 方法只允许设置初始化 store 中的第一级已提前定义好的数据')
-  }
-
-  for (let i = 0, len = arr.length; i < len; i++) {
+  for (let i = 0, len = path.length; i < len; i++) {
     if (i === len - 1) {
-      current[arr[i]] = JSON.parse(JSON.stringify(value))
+      current[path[i]] = JSON.parse(JSON.stringify(value))
     } else {
-      current = current[arr[i]]
+      current = current[path[i]]
     }
   }
 }
@@ -61,56 +62,74 @@ export default class Store {
    * 初始化
    * @param {object} state 全局状态
    */
-  constructor (initState) {
-    state = initState
+  constructor(initState) {
+    this.state = initState
+    this.subs = [] // 页面对象数组
   }
 
   /**
-   * 关联页面
+   * 关联页面，默认将所有全局数据映射给页面 data
    * 如果页面 data 对象中的属性与 state 对象中的属性相同，使用 state 对象中的属性覆盖
    * @param {object} ctx 小程序页面对象
+   * @param {array} 要映射的状态的 key 值数组
    */
-  link (ctx) {
+  mapState(ctx, keys = []) {
     // 避免重复关联
     if (ctx && !ctx.data.__linkId__) {
-      subs.push(ctx)
-      ctx.setData({ __linkId__: linkId++ })
-      subs.forEach(ctx => setState(ctx, state)) // 同步各页面的状态
-    }
-  }
-
-  /**
-   * 取消关联
-   * @param {object} ctx 小程序页面对象
-   */
-  unlink (ctx) {
-    subs.forEach((sub, index) => {
-      if (ctx.data.__linkId__ === sub.data.__linkId__) {
-        subs.splice(index, 1)
-        ctx.setData({ __linkId__: null })
+      linkId = linkId + 1
+      ctx.setData({ __linkId__: linkId })
+      // 选择性映射状态
+      if (keys && keys.length > 0) {
+        const data = {}
+        keys.forEach(stateKey => {
+          Object.assign(data, { [stateKey]: this.state[stateKey] })
+        })
+        this.subs.push({ ctx: ctx, keys: keys, __linkId__: linkId })
+        setState(ctx, data)
+      } else {
+        this.subs.push({ ctx: ctx, __linkId__: linkId })
+        setState(ctx, this.state)
       }
-    })
+    }
   }
 
   /**
    * 更新状态，想要更新全局对象，必须通过 setState 方法
    * @param {object} param
    */
-  setState (param) {
-    Object.keys(param).forEach((key) => {
-      const path = key
-      const value = param[key]
-      if (
-        path !== undefined &&
-        value !== undefined &&
-        typeof path === 'string'
-      ) {
+  setState(param) {
+    Object.keys(param).forEach(key => {
+      const path = getPath(key)
+      // 检查初始化时有没有该状态
+      if (this.state.hasOwnProperty(path[0])) {
+        const value = param[key]
         console.log(`set「${path}」state: `, value)
-        updateByPath(state, path, value) // 更新全局 state，保证新的页面关联时能够得到最新的 state
+        updateByPath(this.state, path, value) // 更新全局 state，保证新的页面关联时能够得到最新的 state
       } else {
         delete param[path]
+        throw new Error(
+          `状态（第一级属性）必须在初始化时就已经声明好：key path：${path}`
+        )
       }
     })
-    subs.forEach(ctx => setState(ctx, param)) // 同步各页面的状态
+
+    // 同步各页面的状态
+    this.subs.forEach(sub => {
+      // 选择性映射
+      if (sub.keys && sub.keys.length > 0) {
+        const data = {}
+        // 当前更新的状态与每个页面所映射的状态做比对，有映射关系的页面就更新
+        sub.keys.forEach(stateKey => {
+          Object.keys(param).forEach(key => {
+            if (getPath(key)[0] === stateKey) {
+              Object.assign(data, { [key]: param[key] })
+            }
+          })
+        })
+        setState(sub.ctx, data)
+      } else {
+        setState(sub.ctx, param)
+      }
+    })
   }
 }
